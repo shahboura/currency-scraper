5230924f6bc9a16e4e60cf82c877046ab7157133
shahboura <shehabelhadidy@hotmail.com>
2016-12-06 03:29:37 +0200

    Closed #6 make use of clusters

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 'use strict'
 
-let express = require('express'),
-	mongoose = require('mongoose'),
-	config = require('./config'),
-	bankScraper = require('./bank-scraper/scraper'),
-	CurrencyModel = require('./models/currencyModel');
+const cluster = require('cluster'),
+numCPUs = require('os').cpus().length;
 
-let app = express();
-let port = process.env.PORT || 3000;
+let express = require('express'),
+mongoose = require('mongoose'),
+config = require('./config'),
+bankScraper = require('./bank-scraper/scraper'),
+CurrencyModel = require('./models/currencyModel');
 
 // use native promises instead of mongoose deprecated
 mongoose.Promise = global.Promise;
-let db = mongoose.connect('mongodb://localhost/currencyAPI');
+mongoose.connect('mongodb://localhost/currencyAPI');
 
-let currencyRouter = require('./routes/currencyRouter')(CurrencyModel);
+if(cluster.isMaster){
+	cluster.on('online', (worker) => {
+		console.log('Yay, the worker responded after it was forked');
+	});
 
-app.use('/api', currencyRouter);
+	// Fork workers.
+	for (var i = numCPUs - 1; i >= 0; i--) {
+		cluster.fork();
+	}
 
-// run server
-app.listen(port, function(){
-	console.log('Running on PORT: ' + port);
-});
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`worker ${worker.process.pid} died, forking...`);
+		cluster.fork();
+	});
 
-bankScraper(config, CurrencyModel).then(results => {
-	console.log('currency rates updated.');
-	console.log(results);
-});
+	// Run only once, no need to scrap in parallel clusters
+	bankScraper(config, CurrencyModel).then(results => {
+		console.log('currency rates updated.');
+		console.log(results);
+	});
+} else {
+	let app = express();
+	let port = process.env.PORT || 3000;
+
+	let currencyRouter = require('./routes/currencyRouter')(CurrencyModel);
+	app.use('/api', currencyRouter);
+
+	// run server
+	app.listen(port, function(){
+		console.log('Running on PORT: ' + port);
+	});
+}
